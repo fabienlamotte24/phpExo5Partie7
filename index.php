cbfff840c574eecf633d5cc4e06517941cb6f45d
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-22 10:30:22 +0200

    Ajout des id sur les input

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
   </head>
   <body>
     <form action="user.php" method="POST"><!--Création formulaire-->
-      <label for="lastName">Votre nom: </label><input type="text" name="lastName" placeholder="nom" /><!--Champs nom de famille-->
-      <label for="firstName">Votre prénom: </label><input type="text" name="firstName" placeholder="prénom" /><!--Champs prénom-->
-      <select name="civility"><!--Champs de sélection du sexe-->
+      <label for="lastName">Votre nom: </label><input type="text" name="lastName" id="lastName" placeholder="nom" /><!--Champs nom de famille-->
+      <label for="firstName">Votre prénom: </label><input type="text" name="firstName" id="firstName" placeholder="prénom" /><!--Champs prénom-->
+      <select name="civility" id="civility"><!--Champs de sélection du sexe-->
         <option selected disabled>Sélectionner votre sexe</option>
         <option value="un homme">Masculin</option><!--Selection masculin-->
         <option value="une femme">Féminin</option><!--Selection féminin-->
